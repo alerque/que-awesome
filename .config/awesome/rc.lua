7b0962aa2fc3aa48862ecb724440262038bf2a87
Caleb Maclennan <caleb@alerque.com>
2014-03-27 14:39:17 +0200

    port old config -- no window borders

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -40,6 +40,8 @@ end
 -- Themes define colours, icons, and wallpapers
 beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
+theme.border_width = "0"
+
 -- This is used later as the default terminal and editor to run.
 terminal = "gnome-terminal"
 browser = "chrome"

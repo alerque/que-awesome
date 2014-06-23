ef119bdc9467f1b75b2a4f3953211b7c553078b7
Caleb Maclennan <caleb@alerque.com>
2014-06-23 14:48:12 +0300

    make default layout tile instead of spiral

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -140,7 +140,7 @@ end
 tags = {}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
-	tags[s] = awful.tag({ " 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 ", " 9 ", " bg " }, s, awful.layout.suit.spiral)
+	tags[s] = awful.tag({ " 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 ", " 9 ", " bg " }, s, awful.layout.suit.tile)
 	tags[s][1].selected = true
 end
 -- }}}

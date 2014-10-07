ed623494416b57225b87310f5e310f66dacaa92c
Caleb Maclennan <caleb@alerque.com>
2014-10-07 16:35:36 +0300

    switched impossibly hard Win+Shift+<dvorak left hand key> to Control for ease of use on Kinesis kbd

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -308,10 +308,9 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey,           }, "w", function () mymainmenu:show() end),
 
     -- Layout manipulation
-    awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end),
-    awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end),
+    awful.key({ modkey, "Control"   }, "j", function () awful.client.swap.byidx(  1)    end),
+    awful.key({ modkey, "Control"   }, "k", function () awful.client.swap.byidx( -1)    end),
     awful.key({ modkey,           }, "o", function () awful.screen.focus_relative(-1) end),
-    --awful.key({ modkey,           }, "u", awful.client.urgent.jumpto),
     awful.key({ modkey,           }, "Tab",
         function ()
             awful.client.focus.history.previous()

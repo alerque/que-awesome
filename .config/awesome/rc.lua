d89b77bbc58b65831855669035670947c4ece0eb
Caleb Maclennan <caleb@alerque.com>
2014-11-29 23:04:46 +0200

    port 'pro' theme into my rc (add Net widget)

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -365,6 +365,29 @@ fswidget = wibox.widget.background()
 fswidget:set_widget(fs_widget)
 fswidget:set_bgimage(beautiful.widget_display)
 
+-- | NET | --
+
+net_widgetdl = wibox.widget.textbox()
+net_widgetul = lain.widgets.net({
+    iface = "enp2s0",
+    settings = function()
+        widget:set_markup(markup.font("Tamsyn 1", "  ") .. net_now.sent)
+        net_widgetdl:set_markup(markup.font("Tamsyn 1", " ") .. net_now.received .. markup.font("Tamsyn 1", " "))
+    end
+})
+
+widget_netdl = wibox.widget.imagebox()
+widget_netdl:set_image(beautiful.widget_netdl)
+netwidgetdl = wibox.widget.background()
+netwidgetdl:set_widget(net_widgetdl)
+netwidgetdl:set_bgimage(beautiful.widget_display)
+
+widget_netul = wibox.widget.imagebox()
+widget_netul:set_image(beautiful.widget_netul)
+netwidgetul = wibox.widget.background()
+netwidgetul:set_widget(net_widgetul)
+netwidgetul:set_bgimage(beautiful.widget_display)
+
 -- }}}
 
 -- {{{ Wibox

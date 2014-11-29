61f87edfdd702e2fb269ec9a6f734331bda23784
Caleb Maclennan <caleb@alerque.com>
2014-11-29 23:04:13 +0200

    port 'pro' theme into my rc (add Mem widget)

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -340,6 +340,20 @@ cpuwidget:set_bgimage(beautiful.widget_display)
 -- tmpwidget:set_widget(tmp_widget)
 -- tmpwidget:set_bgimage(beautiful.widget_display)
 
+-- | MEM | --
+
+mem_widget = lain.widgets.mem({
+    settings = function()
+        widget:set_markup(space3 .. mem_now.used .. "MB" .. markup.font("Tamsyn 4", " "))
+    end
+})
+
+widget_mem = wibox.widget.imagebox()
+widget_mem:set_image(beautiful.widget_mem)
+memwidget = wibox.widget.background()
+memwidget:set_widget(mem_widget)
+memwidget:set_bgimage(beautiful.widget_display)
+
 -- }}}
 
 -- {{{ Wibox

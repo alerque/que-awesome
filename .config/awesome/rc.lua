5bc8def86eadd03a827d77cf826d21c62d2c82df
Caleb Maclennan <caleb@alerque.com>
2014-11-29 23:01:54 +0200

    port 'pro' theme into my rc (add utility widgets section)

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -216,6 +216,23 @@ mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
 menubar.utils.terminal = terminal_plain .. "-e " -- Set the terminal for applications that require it
 -- }}}
 
+-- {{{ Setup utility widgets
+spr = wibox.widget.imagebox()
+spr:set_image(beautiful.spr)
+spr4px = wibox.widget.imagebox()
+spr4px:set_image(beautiful.spr4px)
+spr5px = wibox.widget.imagebox()
+spr5px:set_image(beautiful.spr5px)
+
+widget_display = wibox.widget.imagebox()
+widget_display:set_image(beautiful.widget_display)
+widget_display_r = wibox.widget.imagebox()
+widget_display_r:set_image(beautiful.widget_display_r)
+widget_display_l = wibox.widget.imagebox()
+widget_display_l:set_image(beautiful.widget_display_l)
+widget_display_c = wibox.widget.imagebox()
+widget_display_c:set_image(beautiful.widget_display_c)
+
 -- {{{ Wibox
 -- Create a textclock widget
 mytextclock = awful.widget.textclock()

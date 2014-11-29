f5fe462bb56b2a98b86a1cfcf85b56b34311eab6
Caleb Maclennan <caleb@alerque.com>
2014-11-29 23:03:54 +0200

    port 'pro' theme into my rc (add CPU widget)

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -317,6 +317,29 @@ mailwidget = wibox.widget.background()
 mailwidget:set_widget(mail_widget)
 mailwidget:set_bgimage(beautiful.widget_display)
 
+-- | CPU / TMP | --
+
+cpu_widget = lain.widgets.cpu({
+    settings = function()
+        widget:set_markup(space3 .. cpu_now.usage .. "%" .. markup.font("Tamsyn 4", " "))
+    end
+})
+
+widget_cpu = wibox.widget.imagebox()
+widget_cpu:set_image(beautiful.widget_cpu)
+cpuwidget = wibox.widget.background()
+cpuwidget:set_widget(cpu_widget)
+cpuwidget:set_bgimage(beautiful.widget_display)
+
+-- tmp_widget = wibox.widget.textbox()
+-- vicious.register(tmp_widget, vicious.widgets.thermal, vspace1 .. "$1°C" .. vspace1, 9, "thermal_zone0")
+
+-- widget_tmp = wibox.widget.imagebox()
+-- widget_tmp:set_image(beautiful.widget_tmp)
+-- tmpwidget = wibox.widget.background()
+-- tmpwidget:set_widget(tmp_widget)
+-- tmpwidget:set_bgimage(beautiful.widget_display)
+
 -- }}}
 
 -- {{{ Wibox

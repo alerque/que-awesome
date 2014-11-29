9b9236da2914830b2e944f4f6c3b8a71388c0d5d
Caleb Maclennan <caleb@alerque.com>
2014-11-29 23:03:18 +0200

    port 'pro' theme into my rc (add mail widget)

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -305,6 +305,18 @@ function ()
     awful.util.spawn_with_shell("mpc toggle || ncmpcpp toggle")
     mpdwidget.update()
 end)))
+
+-- | Mail | --
+
+mail_widget = wibox.widget.textbox()
+vicious.register(mail_widget, vicious.widgets.gmail, vspace1 .. "${count}" .. vspace1, 1200)
+
+widget_mail = wibox.widget.imagebox()
+widget_mail:set_image(beautiful.widget_mail)
+mailwidget = wibox.widget.background()
+mailwidget:set_widget(mail_widget)
+mailwidget:set_bgimage(beautiful.widget_display)
+
 -- }}}
 
 -- {{{ Wibox

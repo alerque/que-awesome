62ddb061243c99c41cabc3d5b165b42b92acec6d
Caleb Maclennan <caleb@alerque.com>
2015-02-05 17:22:57 +0200

    comment out mail widget bits

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -327,13 +327,13 @@ end)))
 
 -- | Mail | --
 
-mail_widget = wibox.widget.textbox()
-vicious.register(mail_widget, vicious.widgets.gmail, vspace1 .. "${count}" .. vspace1, 1200)
+--mail_widget = wibox.widget.textbox()
+--vicious.register(mail_widget, vicious.widgets.gmail, vspace1 .. "${count}" .. vspace1, 1200)
 
 widget_mail = wibox.widget.imagebox()
 widget_mail:set_image(beautiful.widget_mail)
 mailwidget = wibox.widget.background()
-mailwidget:set_widget(mail_widget)
+--mailwidget:set_widget(mail_widget)
 mailwidget:set_bgimage(beautiful.widget_display)
 
 -- | CPU / TMP | --

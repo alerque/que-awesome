f6e320ecd0594a78786c85b5aaa788ef7698af9c
Caleb Maclennan <caleb@alerque.com>
2015-04-29 09:57:55 +0300

    update window matching rules for Google Music Mini Player

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -1000,19 +1000,33 @@ root.keys(globalkeys)
 awful.rules.rules = {
     { rule = {
         },
-        properties = { border_width = beautiful.border_width,
-                        border_color = beautiful.border_normal,
-                        focus = awful.client.focus.filter,
-                        keys = clientkeys,
-                        size_hints_honor = false,
-                        buttons = clientbuttons }
+        properties = {
+            border_width = beautiful.border_width,
+            border_color = beautiful.border_normal,
+            focus = awful.client.focus.filter,
+            keys = clientkeys,
+            size_hints_honor = false,
+            buttons = clientbuttons
+        }
     },
     { rule_any = {
-            class = { "Keepassx", "MPlayer", "Shutter" },
+            name = { "Google Play Music Mini Player" },
+        },
+        properties = {
+            floating = true,
+            size_hints_honor = true
+        }
+    },
+    { rule_any = {
+            class = { "Keepassx", "MPlayer", "Shutter", },
+            name = { "Google Play Music Mini Player" },
             instance = { "plugin-container", "exe" },
             role = { "GtkFileChooserDialog" }
         },
-        properties = { floating = true },
+        properties = {
+            floating = true,
+            size_hints_honor = true
+        },
         callback = function(c)
                 awful.placement.centered(c,nil)
             end
